a935e6380f5a33025c05e7dad28ad522bcb1419f
bhrr-alt <bhrr@aluno.ifnmg.edu.br>
2026-04-10 02:38:29 +0000

    feat: pratica funções e métodos de array

--- exercicios/aula06/index.js
+++ exercicios/aula06/index.js
@@ -8,24 +8,41 @@
 // Exercício 1 — criarSaudacao
 export function criarSaudacao(nome) {
   // escreva seu código aqui
+  return `Olá, ${nome}!`;
 }
 
 // Exercício 2 — filtrarAprovados
 export function filtrarAprovados(alunos) {
   // escreva seu código aqui
+  const aprovados = alunos.filter((aluno) => aluno.nota >= 60);
+  return aprovados;
 }
 
 // Exercício 3 — extrairNomes
 export function extrairNomes(alunos) {
   // escreva seu código aqui
+  const nomes = alunos.map((aluno) => aluno.nome);
+  return nomes;
 }
 
 // Exercício 4 — buscarAluno
 export function buscarAluno(alunos, nome) {
   // escreva seu código aqui
+  return nome = alunos.find((aluno) => aluno.nome === nome);
 }
 
 // Exercício 5 — calcularMedia
 export function calcularMedia(notas) {
   // escreva seu código aqui
+   if (notas.length === 0) {
+    return 0
+  }
+
+  let soma = 0
+
+  for (let nota of notas) {
+    soma += nota
+  }
+
+  return soma / notas.length
 }
