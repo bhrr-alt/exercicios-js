8b75861c160234a2905a06917e4dfb55d8093e4c
bhrr-alt <bhrr@aluno.ifnmg.edu.br>
2026-04-10 01:51:26 +0000

    feat: pratica fundamentos de JavaScript no Node - aula 05

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -8,24 +8,61 @@
 // Exercício 1 — classificarIdade
 export function classificarIdade(idade) {
   // escreva seu código aqui
+
+ if (idade < 12) {
+    return "criança";
+  } else if (idade <= 17) {
+    return "adolescente";
+  } else {
+    return "adulto";
+  }
+    
 }
 
 // Exercício 2 — calcularDesconto
 export function calcularDesconto(preco, percentual) {
   // escreva seu código aqui
+  
+  const desconto = preco * percentual /100;
+  const subtração = preco - desconto;
+    return('Valor final é ', subtração);
 }
 
 // Exercício 3 — verificarAprovacao
 export function verificarAprovacao(nota) {
   // escreva seu código aqui
+  if (nota <= 39) {
+    return('reprovado');
+  } else if (nota >= 40, nota < 60) {
+    return('recuperação');
+  } else {
+    return('aprovado');
+  }
 }
 
 // Exercício 4 — somarPares
 export function somarPares(numeros) {
   // escreva seu código aqui
+  let pares = 0;
+  for(const numero of numeros) {
+    if (numero % 2 === 0) {
+      pares += numero;
+    }
+  }
+  return pares;
 }
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
   // escreva seu código aqui
+
+    let contador = 0;
+
+  for (const caractere of texto) {
+    if (caractere === letra) {
+      contador++;
+    }
+  }
+
+  return contador;
 }
